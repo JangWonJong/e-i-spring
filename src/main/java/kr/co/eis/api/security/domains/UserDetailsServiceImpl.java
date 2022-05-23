a946643ef8c11a3caac6dea35dac53fb14348c39
JangWonJong <jwj9922@naver.com>
2022-05-23 15:59:07 +0900

    security2

---
+++ src/main/java/kr/co/eis/api/security/domains/UserDetailsServiceImpl.java
@@ -0,0 +1,15 @@
+package kr.co.eis.api.security.domains;
+
+/**
+ * packageName: kr.co.eis.api.security.domains
+ * fileName   : UserDetailsServiceImpl
+ * author     : Jangwonjong
+ * date       : 2022-05-23
+ * desc       :
+ * ======================================
+ * DATE          AUTHOR            NOTE
+ * ======================================
+ * 2022-05-23     Jangwonjong       최초 생성
+ */
+public class UserDetailsServiceImpl {
+}
